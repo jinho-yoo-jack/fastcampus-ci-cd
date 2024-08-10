cd1ea89082a3a77abbe05b66ec8c406b7c0cc86c
black <jhy7342@gmail.com>
2024-08-10 23:46:17 +0900

    INIT: Spring Boot Project for CI/CD

---
+++ src/main/java/com/fastcampus/cicdstudy/CicdStudyApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.cicdstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdStudyApplication.class, args);
+    }
+
+}
